06fb82d4b862026ea2a0b4030424ac9f699fa164
kimnzh <hakimnizami15@gmail.com>
2025-01-16 23:10:53 +0700

    feat: finalized under development page

--- components/UnderDevelopment/index.tsx
+++ components/UnderDevelopment/index.tsx
@@ -3,11 +3,11 @@ import Logo from "@/public/logo.svg";
 
 const UnderDevelopment = () => {
   return (
-    <main className="flex-col flex items-center pt-52 min-h-screen bg-gradient-to-br from-[#040A27] to-[#020617]">
-      <div className="w-64 h-64 relative">
+    <main className="pt-[25vh] min-h-screen bg-gradient-to-br from-[#040A27] to-[#020617] flex-col flex items-center">
+      <div className="w-48 md:w-64 aspect-square relative">
         <Image src={Logo} alt={"logo"} fill />
       </div>
-      <div className="bg-[#0F172A] border-solid border-2 border-white px-10 py-6 rounded-[32px] font-PoppinsReg text-white mt-12 text-6xl font-semibold glowing-text">
+      <div className="border-solid bg-[#0F172A] border-white border-2 px-4 sm:px-10 py-2 sm:py-6 rounded-[16px] sm:rounded-[32px] text-lg sm:text-3xl lg:text-6xl font-PoppinsReg text-white mt-12 font-semibold glowing-text">
         <span className="flicker-fast">We</span>bs
         <span className="flicker-slow">ite</span> Un
         <span className="flicker-slow">der</span> D
