89007b319a5b6784aaea09bc79432c6aa6e1d38b
kimnzh <hakimnizami15@gmail.com>
2025-01-19 23:51:03 +0700

    feat: navbar testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import UnderDevelopment from "@/components/elements/UnderDevelopment";
 export default function Home() {
   return (
     <main className="dark:bg-tertiary-dark relative h-[2000px] min-h-screen bg-tertiary">
-      {/* <UnderDevelopment /> */}
+      <UnderDevelopment />
     </main>
   );
 }
---
+++ app/testing/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/elements/Navbar";
+
+export default function Page() {
+  return (
+    <main className="dark:bg-tertiary-dark relative h-[2000px] min-h-screen bg-tertiary">
+      <Navbar />
+    </main>
+  );
+}
