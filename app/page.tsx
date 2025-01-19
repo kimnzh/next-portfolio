baba90ece73bdc7e80e4193f0990b334c4626cae
kimnzh <hakimnizami15@gmail.com>
2025-01-19 23:46:04 +0700

    fix: removed old hooks

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import UnderDevelopment from "@/components/elements/UnderDevelopment";
-import Draggable from "@/components/ui/Draggable";
-import Draggable2 from "@/components/ui/Draggable2";
 
 export default function Home() {
   return (
     <main className="dark:bg-tertiary-dark relative h-[2000px] min-h-screen bg-tertiary">
       {/* <UnderDevelopment /> */}
-      {/* <Draggable /> */}
-      {/* <Draggable2 /> */}
     </main>
   );
 }
