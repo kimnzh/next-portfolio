af39cf7ca34db92d8a4d970f969c5579c92f6f4c
kimnzh <hakimnizami15@gmail.com>
2025-01-21 02:59:36 +0700

    fix: fixed vercel deployment requirement

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Providers } from "@/app/providers/providers";
-import Navbar from "@/components/elements/Navbar";
 
 const poppinsBold = localFont({
   src: "./fonts/Poppins-Bold.ttf",
