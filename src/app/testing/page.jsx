28f3489286d58403e1c18d405ef3789d5f9085c5
kimnzh <hakimnizami15@gmail.com>
2025-01-22 01:39:49 +0700

    feat: fixed navbar padding

--- src/app/testing/page.jsx
+++ src/app/testing/page.jsx
@@ -3,9 +3,11 @@ import Logo from "@/components/vectors/Logo";
 
 export default function Page() {
   return (
-    <main className="min-h-screen h-[2000px] font-poppins-reg p-16">
+    <>
       <Navbar />
-      <Logo />
-    </main>
+      <main className="min-h-screen h-[2000px] font-poppins-reg p-16">
+        <Logo />
+      </main>
+    </>
   );
 }
