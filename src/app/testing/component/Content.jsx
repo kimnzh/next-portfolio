c5772da10fb713a8abde86ba24079286235a518d
kimnzh <hakimnizami15@gmail.com>
2025-01-28 10:06:08 +0700

    fix: fixed test routing 5

---
+++ src/app/testing/component/Content.jsx
@@ -0,0 +1,57 @@
+"use client";
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import Navbar from "@/components/elements/Navbar";
+import Footer from "@/components/elements/Footer";
+import Profile from "@/modules/Profile";
+import Experience from "@/modules/Experience";
+import Projects from "@/modules/Projects";
+import Contact from "@/modules/Contact";
+import Life from "@/modules/Life";
+
+const Content = () => {
+  const searchParams = useSearchParams();
+
+  function navigateTo(sectionId) {
+    window.history.pushState({}, "", `/testing#${sectionId}`);
+
+    const section = document.getElementById(sectionId);
+    if (section) {
+      const offset = section.offsetTop;
+      window.scrollTo({
+        top: offset,
+        behavior: "smooth",
+      });
+    }
+  }
+
+  useEffect(() => {
+    const hash = window.location.hash.replace("#", "");
+    if (hash) {
+      const section = document.getElementById(hash);
+      if (section) {
+        setTimeout(() => {
+          section.scrollIntoView({ behavior: "smooth" });
+        }, 100);
+      }
+    } else {
+    }
+  }, [searchParams]);
+
+  return (
+    <>
+      <Navbar onNavigate={navigateTo} />
+      <main className="min-h-screen max-w-[1440px] w-full overflow-hidden px-8 sm:px-12 xl:px-24">
+        <Profile />
+        <Experience />
+        <Projects />
+        <Life />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Content;
--- src/app/testing/page.jsx
+++ src/app/testing/page.jsx
@@ -1,55 +1,11 @@
-"use client";
-import { useEffect } from "react";
-import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
-import Navbar from "@/components/elements/Navbar";
-import Footer from "@/components/elements/Footer";
-import Profile from "@/modules/Profile";
-import Experience from "@/modules/Experience";
-import Projects from "@/modules/Projects";
-import Contact from "@/modules/Contact";
-import Life from "@/modules/Life";
+
+import Content from "./component/Content";
 
 export default function Page() {
-  const searchParams = useSearchParams();
-
-  function navigateTo(sectionId) {
-    window.history.pushState({}, "", `/testing#${sectionId}`);
-
-    const section = document.getElementById(sectionId);
-    if (section) {
-      const offset = section.offsetTop;
-      window.scrollTo({
-        top: offset,
-        behavior: "smooth",
-      });
-    }
-  }
-
-  useEffect(() => {
-    const hash = window.location.hash.replace("#", "");
-    if (hash) {
-      const section = document.getElementById(hash);
-      if (section) {
-        setTimeout(() => {
-          section.scrollIntoView({ behavior: "smooth" });
-        }, 100);
-      }
-    } else {
-    }
-  }, [searchParams]);
-
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Navbar onNavigate={navigateTo} />
-      <main className="min-h-screen max-w-[1440px] w-full overflow-hidden px-8 sm:px-12 xl:px-24">
-        <Profile />
-        <Experience />
-        <Projects />
-        <Life />
-        <Contact />
-      </main>
-      <Footer />
+    <Suspense>
+      <Content />
     </Suspense>
   );
 }
